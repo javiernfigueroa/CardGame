1f3946a3663268e5d0a4c034df3c9f7c4a0fecea
Javier Figueroa <109968115+javiernfigueroa@users.noreply.github.com>
2023-04-13 19:52:38 -0400

    update

---
+++ data.js
@@ -0,0 +1,20 @@
+const hero = {
+    elementId: "hero",
+    name: "Wizard",
+    avatar: "images/wizard.png",
+    health: "60",
+    diceRoll: 6,
+  };
+  
+const monster = {
+    elementId: "monster",
+    name: "Orc",
+    avatar: "images/orc.png",
+    health: "10",
+    diceRoll: 4,
+  };
+
+  module.exports = {
+    hero,
+    monster
+  };
--- index.js
+++ index.js
@@ -1,7 +1,51 @@
-/*
-Challenge: 
-1. Take the hard-coded HTML for the Wizard card, bring it 
-   into index.js and then inject it back into its div with 
-   JavaScript.
-2. Do the same for Orc card. 
-*/
+// import { hero } from "./data";
+// import { monster } from "./data";
+
+const hero = {
+  elementId: "hero",
+  name: "Wizard",
+  avatar: "images/wizard.png",
+  health: "60",
+  diceRoll: [3, 1, 4],
+  diceCount: 3,
+};
+
+const monster = {
+  elementId: "monster",
+  name: "Orc",
+  avatar: "images/orc.png",
+  health: "10",
+  diceRoll: [2],
+  diceCount: 1,
+};
+
+function getDiceRollArray(diceCount){
+   const newDiceRolls = []
+   for(let i = 0; i < diceCount; i++){
+      newDiceRolls.push(Math.floor(Math.random()*6)+1)
+   }
+   return newDiceRolls
+}
+
+getDiceRollArray(3)
+
+function renderCharacter(data) {
+  const { elementId, name, avatar, health, diceRoll, diceCount } = data;
+
+  let diceHtml = diceRoll.map(function (num) {
+    return `
+    <div class="dice">${num}</div>
+    `;
+  }).join(' ');
+  
+  document.getElementById(elementId).innerHTML = `<div class="character-card">
+         <h4 class="name"> ${name} </h4>
+         <img class="avatar" src="${avatar}"/>
+         <p class="health">health: <b> ${health}</b></p>
+         <div class="dice-container">
+         ${diceHtml}</div>
+     </div>`;
+}
+
+renderCharacter(hero);
+renderCharacter(monster);
