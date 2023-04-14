e37971e9feb3a4f4d759f5ca649c0b67e887feef
Javier Figueroa <109968115+javiernfigueroa@users.noreply.github.com>
2023-04-14 18:25:22 -0400

    update

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import characterData from "/data.js";
 import Character from "/Character.js";
 
 function render() {
-  document.getElementById(wizard.elementId).innerHTML =
+  document.getElementById('hero').innerHTML =
     wizard.getCharacterHtml();
-  document.getElementById(orc.elementId).innerHTML = orc.getCharacterHtml();
+  document.getElementById('monster').innerHTML = orc.getCharacterHtml();
 }
 const wizard = new Character(characterData.hero);
 const orc = new Character(characterData.monster);
