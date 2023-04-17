4a5e9cdda6efe065062a70fc15bc7f6a5724fe69
Javier Figueroa <109968115+javiernfigueroa@users.noreply.github.com>
2023-04-17 10:16:21 -0400

    update

--- utils.js
+++ utils.js
@@ -1,9 +1,13 @@
-
 function getDiceRollArray(diceCount) {
-    const newDiceRolls = new Array(diceCount).fill(0).map(function () {
-      return Math.floor(Math.random() * 6) + 1;
-    });
-    return newDiceRolls;
-  }
+  return new Array(diceCount).fill(0).map(function(){
+  return Math.floor(Math.random() * 6) + 1
+  });   
+}
 
-  export { getDiceRollArray }
+function getDicePlaceholderHtml(diceCount){
+  return new Array(diceCount).fill(0).map(function(){
+      return `<div class="placeholder-dice"></div>`
+  }).join('')
+}
+
+export {getDiceRollArray, getDicePlaceholderHtml}
